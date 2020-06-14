23bffd1db614dc79146f908698a162ab1538e915
geekypandey <anuragcarbon1998@gmail.com>
2020-06-14 18:39:06 +0530

    improve code quality

--- download_covid19_sitrep.py
+++ download_covid19_sitrep.py
@@ -1,22 +1,25 @@
 import os
 import time
 import requests
-from bs4 import BeautifulSoup as bs 
+from bs4 import BeautifulSoup as bs
+
 
 URL = 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports'
 
-def get_report(url=URL):
+
+def get_report():
     """
     Download situation report from who.int for further analysis of data
     """
-    
-    res = requests.get(url)
+
+    res = requests.get(URL)
     soup = bs(res.text, 'html.parser')
     all_links = soup.find_all('a')
-    report_links = [link.get('href') for link in all_links if 'Situation' in link.text]
+    report_links = (link.get('href') for link in all_links if 'Situation' in link.text)
+    base_url = 'www.who.int'
     for link in report_links:
         time.sleep(5)
-        os.system(f'wget -nc www.who.int{link}')
+        os.system(f'wget -nc {base_url + link}')
 
 
 if __name__ == '__main__':
