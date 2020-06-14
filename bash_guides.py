fe83be1f1cd6c86e30e91f51a77793905dcc7c1f
Anurag Pandey <anuragcarbon1998@gmail.com>
2020-06-14 18:34:58 +0530

    improve code quality
    Using generator expression instead of list.
    Maintaining a style order.

--- bash_guides.py
+++ bash_guides.py
@@ -3,15 +3,20 @@ import os
 import requests
 from bs4 import BeautifulSoup as bs
 
-def main():
-    """ This scripts downloads all the guides from the given URL in PDF format.The page contains the guides in many different formats"""
 
+
+
+def main():
+    """ This scripts downloads all the guides from the given URL in PDF format.
+    The page contains the guides in many different formats"""
+    
     URL = "http://tldp.org/guides.html"
     res = requests.get(URL)
-    soup = bs(res.text,'html.parser')
-    links = ["https://tldp.org/" + link.a['href'] for link in soup.find_all('li') if link.a.text.strip() == "PDF"]
+    soup = bs(res.text, 'html.parser')
+    base_url = 'https://tldp.org/'
+    links = (base_url + link.a['href'] for link in soup.find_all('li') if link.a.text.strip() == "PDF")
     for link in links:
-        os.system(f"wget -nc {link}") #using wget to download
+        os.system(f"wget -nc {link}")  #using wget to download
 
 if __name__ == "__main__":
     main()
